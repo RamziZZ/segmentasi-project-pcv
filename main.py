1419cbc4970e61f272cff83547f43e8cf4f8ee5e
RamziZZ <ramzifarhan072@gmail.com>
2025-10-29 19:39:49 +0700

    bg remove new

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+import os
 from src.background_remover import SmartBackgroundRemover
 
 if __name__ == "__main__":
-    input_path = "data/input/sehat_20.png"
-    output_path_white = "data/output/sehat_20.png"
-    # output_path_transparent = "data/output/hasil_transparan.png"
+    input_folder = "data/input"
+    output_folder = "data/output2"
 
     remover = SmartBackgroundRemover()
 
-    # === Hasil background putih ===
-    remover.remove_background(input_path, output_path_white, white_bg=True)
+    # Pastikan folder output ada
+    os.makedirs(output_folder, exist_ok=True)
 
-    # === Hasil background transparan (opsional) ===
-    # remover.remove_background(input_path, output_path_transparent, white_bg=False)
+    # Loop semua file di folder input
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename)
+
+            print(f"[🔄] Memproses: {filename} ...")
+            remover.remove_background(input_path, output_path, transparan_bg=True)
+            print(f"[✅] Selesai → {output_path}")
+
+    print("\n🎉 Semua gambar selesai diproses!")
